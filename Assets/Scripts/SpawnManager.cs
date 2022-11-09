65dc2c9aa6502eeba1080d5a8a62004adfb6ccd6
billy pesoth <billy.pesoth@nadihealth.com>
2022-11-09 10:21:19 +0800

    add new method spawn animals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,24 +9,29 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // After startDelay, will call method SpawnRandomAnimal every spawnInterval
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+       
+    }
 
-            Instantiate(animalPrefabs[animalIndex],
-                spawnPosition,
-                animalPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = UnityEngine.Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex],
+            spawnPosition,
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
