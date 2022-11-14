0472f2d4f63aa0e01f458582114994caa9193e31
billybep <billy.pesoth@gmail.com>
2022-11-14 09:11:27 +0800

    add pooling object for optimize

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -12,11 +13,16 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float spawnInterval = 0.5f;
 
+    public float spawnMinRangeZ = -1.0f;
+    public float spawnMaxRangeZ = 17.0f;
+    public float sideSpawn = 16.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         // After startDelay, will call method SpawnRandomAnimal every spawnInterval
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnLeft", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -33,5 +39,18 @@ public class SpawnManager : MonoBehaviour
         Instantiate(animalPrefabs[animalIndex],
             spawnPosition,
             animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    void SpawnLeft()
+    {
+        int animalIndex = UnityEngine.Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPositionSide = new Vector3(-sideSpawn, 0, UnityEngine.Random.Range(spawnMinRangeZ, spawnMaxRangeZ));
+        Vector3 rotateRight = new Vector3(0, 90, 0);
+
+        Instantiate(animalPrefabs[animalIndex],
+            spawnPositionSide,
+            Quaternion.Euler(rotateRight));
+
+        UnityEngine.Debug.Log("Left");
     }
 }
